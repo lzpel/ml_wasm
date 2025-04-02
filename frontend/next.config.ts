7887cdcd6543d38861203736e9950fe6c9e8522b
lzpel <18492524+lzpel@users.noreply.github.com>
2025-04-02 13:19:38 +0900

    workflow fix path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,12 @@
 import type {NextConfig} from "next";
 
+const GITHUB_REPO = process.env.GITHUB_PAGES ? "ml-wasm" : undefined;
+
 const nextConfig: NextConfig = {
 	// output: 'standalone', これは動的なサイトを生成してしまうので間違い
 	output: "export",
+	basePath: GITHUB_REPO ? `/${GITHUB_REPO}` : undefined,
+	assetPrefix: GITHUB_REPO ? `/${GITHUB_REPO}/` : undefined,
 	webpack: (config, options) => {
 		config.experiments = {
 			...config.experiments,
