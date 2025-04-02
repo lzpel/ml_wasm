84adae3871a0837ad03c6c98dc5a4501331c73db
lzpel <18492524+lzpel@users.noreply.github.com>
2025-04-02 15:13:28 +0900

    workflow add output: export

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 const GITHUB_REPO = process.env.GITHUB_PAGES ? "ml-wasm" : undefined;
 // next.config.tsはworkflowが読み込めない。mjsが回避策
 const nextConfig = {
-	// output: 'standalone', これは動的なサイトを生成してしまうので間違いでexportが正解。そしてmjsにするならばexportも書かなくてよい
+	output: 'export', // これは動的なサイトを生成してしまうので間違いでexportが正解。そしてmjsにするならばexportも書かなくてよい
 	basePath: GITHUB_REPO ? `/${GITHUB_REPO}` : undefined,
 	assetPrefix: GITHUB_REPO ? `/${GITHUB_REPO}/` : undefined,
 	webpack: (config, options) => {
